e0893559432d7252d30f0f02cf515caddaaee61d
Sam Seifert <seifes1@gmail.com>
2015-09-13 20:42:17 -0400

    Quaternion Extensions

---
+++ Extensions.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using OpenTK;
+
+namespace SamSeifert.GLE
+{
+    public static class Extensions
+    {
+        public static Quaternion QuaternionFromEulerAnglesDegrees(float heading, float attitude, float bank)
+        {
+            return new Quaternion(
+                MathHelper.DegreesToRadians(attitude),
+                MathHelper.DegreesToRadians(heading),
+                MathHelper.DegreesToRadians(bank)
+                );
+        }
+
+        public static Quaternion QuaternionFromTaitBrynDegrees(float yaw, float pitch, float roll)
+        {
+            
+            return Quaternion.Multiply(Quaternion.Multiply(
+                Quaternion.FromAxisAngle(Vector3.UnitZ, MathHelper.DegreesToRadians(yaw)),
+                Quaternion.FromAxisAngle(Vector3.UnitY, MathHelper.DegreesToRadians(pitch))),
+                Quaternion.FromAxisAngle(Vector3.UnitX, MathHelper.DegreesToRadians(roll)));
+        }
+
+    }
+}
