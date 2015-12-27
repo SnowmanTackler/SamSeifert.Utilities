cc728dfc2b068f17b01fbc131b94984c9ad49f85
Sam Seifert <seifes1@gmail.com>
2015-12-27 13:09:57 -0500

    Initial Commit

---
+++ ControlUtil.cs
@@ -0,0 +1,71 @@
+﻿using SamSeifert.Utilities;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SamSeifert.Utilities
+{
+    public static class ControlUtil
+    {
+        public static void WireAllControls_MouseDown(this Control parent, MouseEventHandler eh)
+        {
+            parent.ToggleAll_MouseDown(eh, true);
+        }
+
+        public static void UnwireAllControls_MouseDown(this Control parent, MouseEventHandler eh)
+        {
+            parent.ToggleAll_MouseDown(eh, false);
+        }
+
+        private static void ToggleAll_MouseDown(this Control parent, MouseEventHandler eh, bool add)
+        {
+            if (add) parent.MouseDown += eh;
+            else parent.MouseDown -= eh;
+            foreach (Control child in parent.Controls) child.ToggleAll_MouseDown(eh, add);
+        }
+
+        public static void CascadeContextMenuStrip(this Control parent)
+        {
+            parent.CascadeContextMenuStrip(parent.ContextMenuStrip);
+        }
+
+        public static void DecascadeContextMenuStrip(this Control parent)
+        {
+            parent.CascadeContextMenuStrip(null);
+        }
+
+        private static void CascadeContextMenuStrip(this Control parent, ContextMenuStrip cms)
+        {
+            foreach (Control child in parent.Controls)
+            {
+                child.ContextMenuStrip = cms;
+                if (child.HasChildren) child.CascadeContextMenuStrip(cms);
+            }
+        }
+
+        public static bool ContainsScreenPoint(this Control c, Point p)
+        {
+            p = c.PointToClient(p);
+            if (p.X < 0) return false;
+            if (p.Y < 0) return false;
+            if (p.X >= c.Width) return false;
+            if (p.Y >= c.Height) return false;
+            return true;
+        }
+
+        public static void RemoveFromParent(this Control c)
+        {
+            if (c.Parent == null) return;
+            c.Parent.Controls.Remove(c);
+        }
+
+
+
+
+
+    }
+}
---
+++ DoubleBuffered.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace SamSeifert.Utilities
+{
+    public class DoubleBufferedPanel : Panel
+    {
+        public DoubleBufferedPanel() : base()
+        {
+        }
+
+        private Bitmap _Bitmap;
+        private Graphics _Graphics;
+
+        protected override void OnResize(EventArgs eventargs)
+        {
+            if (this.DesignMode) return;
+
+            base.OnResize(eventargs);
+
+            if (this._Bitmap == null)
+            {
+                if (this._Bitmap.Size == base.Size) return;
+                this._Graphics.Dispose();
+                this._Bitmap.Dispose();
+            }
+
+            this._Bitmap = new Bitmap(base.Width, base.Height, PixelFormat.Format24bppRgb);
+            this._Graphics = Graphics.FromImage(this._Bitmap);
+        }
+
+        protected override void OnPaintBackground(PaintEventArgs e)
+        {
+//            base.OnPaintBackground(e);
+        }
+
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            if (this.DesignMode) return;
+
+            base.OnPaint(new PaintEventArgs(this._Graphics,
+                new Rectangle(0, 0, this._Bitmap.Width, this._Bitmap.Height)));
+
+            e.Graphics.DrawImage(this._Bitmap, 0, 0);
+        }
+    }
+
+    public partial class DoubleBufferedForm : Form
+    {
+        public DoubleBufferedForm() : base()
+        {
+            this.Load += ControlLoaded;
+        }
+
+        private void ControlLoaded(object sender, EventArgs e)
+        {
+            this.updateBitmap();
+        }
+
+        private Bitmap _Bitmap;
+        private Graphics _Graphics;
+        private PaintEventArgs _PaintEventArgs;
+
+        protected override void OnResize(EventArgs eventargs)
+        {
+            base.OnResize(eventargs);
+        }
+
+        private void updateBitmap()
+        {
+            if (this._Bitmap != null) this._Bitmap.Dispose();
+            this._Bitmap = new Bitmap(this.Width, this.Height, PixelFormat.Format24bppRgb);
+            this._Graphics = Graphics.FromImage(this._Bitmap);
+            this._PaintEventArgs = new PaintEventArgs(this._Graphics, new Rectangle(
+                0,
+                0,
+                this.ClientSize.Width,
+                this.ClientSize.Height));
+        }
+
+        protected override void OnPaintBackground(PaintEventArgs e)
+        {
+            //            base.OnPaintBackground(e);
+        }
+
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            this._Graphics.ResetClip();
+            this._Graphics.ResetTransform();
+            base.OnPaint(this._PaintEventArgs);
+            e.Graphics.DrawImage(this._Bitmap, 0, 0);
+        }
+    }
+}
