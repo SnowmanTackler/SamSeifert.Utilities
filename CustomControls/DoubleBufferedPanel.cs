263820886bf8c5e7b0e5253e56fc788f318f8bee
Sam Seifert <seifes1@gmail.com>
2016-01-07 11:56:08 -0500

    Added ColorMethods and HueChooser

---
+++ CustomControls/DoubleBufferedPanel.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace SamSeifert.Utilities.CustomControls
+{
+    public class DoubleBufferedPanel : Panel
+    {
+        private Bitmap _Bitmap;
+        private Graphics _Graphics;
+        private PaintEventArgs _PaintEventArgs;
+
+        public DoubleBufferedPanel() : base()
+        {
+            this.OnResize(EventArgs.Empty);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (this._Bitmap != null)
+            {
+                this._PaintEventArgs.Dispose();
+                this._Graphics.Dispose();
+                this._Bitmap.Dispose();
+            }
+
+            base.Dispose(disposing);
+        }
+
+        protected override void OnResize(EventArgs eventargs)
+        {
+            base.OnResize(eventargs);
+
+            if (this.DesignMode) return;
+
+            if (this._Bitmap != null)
+            {
+                if (this._Bitmap.Size == base.Size) return;
+                this._PaintEventArgs.Dispose();
+                this._Graphics.Dispose();
+                this._Bitmap.Dispose();
+            }
+
+            this._Bitmap = new Bitmap(base.Width, base.Height, PixelFormat.Format24bppRgb);
+            this._Graphics = Graphics.FromImage(this._Bitmap);
+            this._PaintEventArgs = new PaintEventArgs(this._Graphics, new Rectangle(
+                0,
+                0,
+                this.Width,
+                this.Height));
+        }
+
+        protected override void OnPaintBackground(PaintEventArgs e)
+        {
+            if (this.DesignMode) return;
+            this._Graphics.ResetClip();
+            this._Graphics.ResetTransform();
+            base.OnPaintBackground(this._PaintEventArgs);
+        }
+
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            if (this.DesignMode) return;
+            this._Graphics.ResetClip();
+            this._Graphics.ResetTransform();
+            base.OnPaint(this._PaintEventArgs);
+            e.Graphics.DrawImage(this._Bitmap, 0, 0);
+        }
+    }
+}
---
+++ CustomControls/HueChooser.Designer.cs
@@ -0,0 +1,181 @@
+﻿namespace SamSeifert.Utilities.CustomControls
+{
+    partial class HueChooser
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.nudBandWidth = new System.Windows.Forms.NumericUpDown();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.nudBandCenter = new System.Windows.Forms.NumericUpDown();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nudBandWidth)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nudBandCenter)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.pictureBox1.BackColor = System.Drawing.SystemColors.Control;
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(325, 97);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.SizeChanged += new System.EventHandler(this.pictureBox1_SizeChanged);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.checkBox1.CheckAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.checkBox1.Checked = true;
+            this.checkBox1.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.checkBox1.Font = new System.Drawing.Font("Tahoma", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.checkBox1.ForeColor = System.Drawing.Color.Black;
+            this.checkBox1.Location = new System.Drawing.Point(3, 162);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(166, 20);
+            this.checkBox1.TabIndex = 1;
+            this.checkBox1.Text = "Grayscale Background:";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.changed);
+            // 
+            // nudBandWidth
+            // 
+            this.nudBandWidth.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.nudBandWidth.Font = new System.Drawing.Font("Tahoma", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.nudBandWidth.Location = new System.Drawing.Point(109, 133);
+            this.nudBandWidth.Maximum = new decimal(new int[] {
+            360,
+            0,
+            0,
+            0});
+            this.nudBandWidth.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.nudBandWidth.Name = "nudBandWidth";
+            this.nudBandWidth.Size = new System.Drawing.Size(60, 23);
+            this.nudBandWidth.TabIndex = 1;
+            this.nudBandWidth.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.nudBandWidth.Value = new decimal(new int[] {
+            100,
+            0,
+            0,
+            0});
+            this.nudBandWidth.ValueChanged += new System.EventHandler(this.changed);
+            // 
+            // label2
+            // 
+            this.label2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.label2.Font = new System.Drawing.Font("Tahoma", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.Black;
+            this.label2.Location = new System.Drawing.Point(3, 132);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(100, 23);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Band Width:";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.label1.Font = new System.Drawing.Font("Tahoma", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Black;
+            this.label1.Location = new System.Drawing.Point(3, 104);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(100, 23);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Band Center:";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // nudBandCenter
+            // 
+            this.nudBandCenter.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.nudBandCenter.Font = new System.Drawing.Font("Tahoma", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.nudBandCenter.Location = new System.Drawing.Point(109, 104);
+            this.nudBandCenter.Maximum = new decimal(new int[] {
+            360,
+            0,
+            0,
+            0});
+            this.nudBandCenter.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            -2147483648});
+            this.nudBandCenter.Name = "nudBandCenter";
+            this.nudBandCenter.Size = new System.Drawing.Size(60, 23);
+            this.nudBandCenter.TabIndex = 1;
+            this.nudBandCenter.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.nudBandCenter.Value = new decimal(new int[] {
+            300,
+            0,
+            0,
+            0});
+            this.nudBandCenter.ValueChanged += new System.EventHandler(this.changed);
+            // 
+            // HueChooser
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.nudBandCenter);
+            this.Controls.Add(this.nudBandWidth);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.checkBox1);
+            this.Name = "HueChooser";
+            this.Size = new System.Drawing.Size(325, 187);
+            this.BackColorChanged += new System.EventHandler(this.HueChooser_BackColorChanged);
+            this.Resize += new System.EventHandler(this.HueChooser_Resize);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nudBandWidth)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nudBandCenter)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.NumericUpDown nudBandWidth;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.NumericUpDown nudBandCenter;
+
+    }
+}
