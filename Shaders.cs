1fcc71ac64adb4452c8fbb5c05a8bece6e4a10aa
Sam Seifert <seifes1@gmail.com>
2016-01-31 13:30:16 -0500

    Shaders auto finds Uniforms

--- Shaders.cs
+++ Shaders.cs
@@ -4,65 +4,311 @@ using System.Linq;
 using System.Text;
 
 using OpenTK;
-using OpenTK.Graphics.OpenGL; using GL = SamSeifert.GLE.GLR;
+using OpenTK.Graphics.OpenGL;
+using GLO = OpenTK.Graphics.OpenGL.GL;
+using GL = SamSeifert.GLE.GLR;
 
 namespace SamSeifert.GLE
 {
     public class Shaders
     {
-        public static bool CreateShaders(
-            String vertexShader, 
-            String fragmentShader,
-            out int program,
-            out int vshader,
-            out int fshader)
+        private readonly int _GL_Program = 0;
+        private readonly int _GL_Vertex = 0;
+        private readonly int _GL_Frag = 0;
+        private Dictionary<String, int> UniformLocations = new Dictionary<String, int>();
+
+        public void UseProgram()
         {
-            int vertShader = createVertShader(vertexShader);
-            int fragShader = createFragShader(fragmentShader);
+            GL.UseProgram(this._GL_Program);
+        }
 
-            if (vertShader != 0 && fragShader != 0)
+        private int UniformLocation(String key)
+        {
+            int of_the_king;
+            if (this.UniformLocations.TryGetValue(key, out of_the_king))
             {
-                int shader = GL.CreateProgram();
-
-                GL.AttachShader(shader, vertShader);
-                GL.AttachShader(shader, fragShader);
-
-                int param;
-
-                GL.LinkProgram(shader);
-                GL.GetProgram(shader, ProgramParameter.LinkStatus, out param);
-                if (param == 0)
-                {
-                    Console.WriteLine("Linkage error");
-                    program = 0; vshader = 0; fshader = 0;
-                    GL.DeleteShader(vertShader);
-                    GL.DeleteShader(fragShader);
-                    return false;
-                }
-
-                GL.ValidateProgram(shader);
-                GL.GetProgram(shader, ProgramParameter.ValidateStatus, out param);
-                if (param == 0)
-                {
-                    Console.WriteLine("Validate error");
-                    program = 0; vshader = 0; fshader = 0;
-                    GL.DeleteShader(vertShader);
-                    GL.DeleteShader(fragShader);
-                    return false;
-                }
-
-                vshader = vertShader;
-                fshader = fragShader;
-                program = shader;
-
-                return true;
+                return of_the_king;
             }
             else
             {
-                program = 0; vshader = 0; fshader = 0;
-                if (vertShader != 0) GL.DeleteShader(vertShader);
-                if (fragShader != 0) GL.DeleteShader(fragShader);
-                return false;
+                Console.WriteLine("Error - Shaders - No uniform for key:" + key);
+                return 0;
+            }
+        }
+
+        public bool SuccessfulSetup
+        {
+            get
+            {
+                return this._GL_Program != 0;
+            }
+        }
+
+        public Shaders(String vertexShader, String fragmentShader)
+        {
+            try
+            {
+                this._GL_Vertex = createVertShader(vertexShader);
+                this._GL_Frag = createFragShader(fragmentShader);
+
+                if (this._GL_Vertex != 0 && this._GL_Frag != 0)
+                {
+                    int shader = GL.CreateProgram();
+
+                    GL.AttachShader(shader, this._GL_Vertex);
+                    GL.AttachShader(shader, this._GL_Frag);
+
+                    int param;
+
+                    GL.LinkProgram(shader);
+                    GL.GetProgram(shader, ProgramParameter.LinkStatus, out param);
+                    if (param == 0)
+                    {
+                        Console.WriteLine("Linkage error");
+                        return;
+                    }
+
+                    GL.ValidateProgram(shader);
+                    GL.GetProgram(shader, ProgramParameter.ValidateStatus, out param);
+                    if (param == 0)
+                    {
+                        Console.WriteLine("Validate error");
+                        return;
+                    }
+
+                    this._GL_Program = shader;
+                }
+            }
+            finally
+            {
+                if (this._GL_Program == 0)
+                {
+                    if (this._GL_Vertex != 0) GL.DeleteShader(this._GL_Vertex);
+                    if (this._GL_Frag != 0) GL.DeleteShader(this._GL_Frag);
+                }
+                else
+                {
+                    var uniforms = new HashSet<string>();
+                    foreach (string s in Shaders.FindUniforms(Shaders.PurgeComments(vertexShader)))
+                        uniforms.Add(s);
+                    foreach (string s in Shaders.FindUniforms(Shaders.PurgeComments(fragmentShader)))
+                        uniforms.Add(s);
+
+                    foreach (String s in uniforms)
+                        this.UniformLocations[s] = GL.GetUniformLocation(this._GL_Program, s);
+                }
+
+                GL.UseProgram(0);
+            }
+        }
+
+        /// <summary>
+        /// What active textures correspond to what uniforms
+        /// </summary>
+        /// <param name="uniform_name"></param>
+        /// <param name="texture_index"></param>
+        public void setTextureLocation(String uniform_name, int texture_index)
+        {
+            GL.UseProgram(this._GL_Program);
+            GLO.Uniform1(this.UniformLocation(uniform_name), texture_index);
+            GL.UseProgram(0);
+        }
+
+
+        public void Uniform(String uniform_name, float f)
+        {
+            GLO.Uniform1(this.UniformLocation(uniform_name), f);
+        }
+
+        public void Uniform(String uniform_name, ref Vector2 v)
+        {
+            GLO.Uniform2(this.UniformLocation(uniform_name), ref v);
+        }
+
+        public void Uniform(String uniform_name, ref Vector3 v)
+        {
+            GLO.Uniform3(this.UniformLocation(uniform_name), ref v);
+        }
+
+        public void Uniform(String uniform_name, ref Matrix4 mat)
+        {
+            GLO.UniformMatrix4(this.UniformLocation(uniform_name), false, ref mat);
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        private static string PurgeComments(String s)
+        {
+            var sb = new StringBuilder();
+
+            bool in_line_comment = false;
+            bool in_multiline_comment = false;
+
+            var char_array = s.ToCharArray();
+            for (int i = 0; i < char_array.Length; i++)
+            {
+                char c = char_array[i];
+
+                if (in_line_comment)
+                {
+                    switch (c)
+                    {
+                        case '\n':
+                            in_line_comment = false;
+                            sb.Append(c);
+                            break;
+                    }
+                }
+                else if (in_multiline_comment)
+                {
+                    if (i < char_array.Length - 1) // two char features
+                    {
+                        switch (c)
+                        {
+                            case '*':
+                                switch (char_array[i + 1])
+                                {
+                                    case '/':
+                                        i++;
+                                        in_multiline_comment = false;
+                                        break;
+                                }
+                                break;
+                        }
+                    }
+                }
+                else
+                {
+                    if (i < char_array.Length - 1) // two char features
+                    {
+                        switch (c)
+                        {
+                            case '/':
+                                switch (char_array[i + 1])
+                                {
+                                    case '/':
+                                        i++;
+                                        in_line_comment = true;
+                                        break;
+                                    case '*':
+                                        i++;
+                                        in_multiline_comment = true;
+                                        break;
+                                    default:
+                                        sb.Append(c);
+                                        break;
+                                }
+                                break;
+                            default:
+                                sb.Append(c);
+                                break;
+                        }
+                    }
+                    else sb.Append(c);
+                }
+            }
+
+            String of_the_jedi = sb.ToString();
+
+            /*
+            Console.WriteLine(" CONVERT ");
+            Console.WriteLine();
+            Console.WriteLine(s);
+            Console.WriteLine();
+            Console.WriteLine(" TO ");
+            Console.WriteLine();
+            Console.WriteLine(of_the_jedi);
+            Console.WriteLine();
+            Console.WriteLine(" DONE ");
+            */
+
+            return of_the_jedi;
+        }
+
+        private static IEnumerable<string> FindUniforms(string text)
+        {
+            int counter = 0;
+
+            var white_space = new char[] { ' ', '\t', '\n', ';' };
+
+            foreach (var s in text.Split(white_space, StringSplitOptions.RemoveEmptyEntries))
+            {
+                var trim = s.Trim();
+                if (trim.Length != 0)
+                {
+                    counter--;
+                    switch (trim)
+                    {
+                        case "uniform":
+                            counter = 2;
+                            break;
+                        default:
+                            if (counter == 0) yield return trim; // var name 2 after uniform
+                            break;
+                    }
+                }
             }
         }
 
