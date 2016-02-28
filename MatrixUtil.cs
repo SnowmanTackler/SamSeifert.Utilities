1bb686e07bcf7d6b14ebe004e7fda33f23e43fae
Sam Seifert <seifes1@gmail.com>
2016-02-27 22:20:09 -0500

    Data Util

--- MatrixUtil.cs
+++ MatrixUtil.cs
@@ -14,35 +14,196 @@ namespace SamSeifert.MathNet.Numerics.Extensions
             int rows = m.RowCount;
             int cols = m.ColumnCount;
 
-            float[] means = new float[cols];
-
-            for (int c = 0; c < cols; c++)
-            {
-                means[c] = 0;
-                for (int r = 0; r < rows; r++) means[c] += m[r, c];
-                means[c] /= rows;
-            }
-
+            var means = m.MeanRow();
 
             var of_the_king = Matrix<float>.Build.Dense(cols, cols, 0);
 
-            for (int var1 = 0; var1 < cols; var1++)
+            for (int c1 = 0; c1 < cols; c1++)
             {
-                for (int var2 = var1; var2 < cols; var2++)
+                for (int c2 = c1; c2 < cols; c2++)
                 {
                     float sum = 0;
 
                     for (int r = 0; r < rows; r++)
-                        sum += (m[r, var1] - means[var1]) * (m[r, var2] - means[var2]);
+                        sum += (m[r, c1] - means[c1]) * (m[r, c2] - means[c2]);
 
                     sum /= (rows - 1);
 
-                    of_the_king[var1, var2] = sum;
-                    of_the_king[var2, var1] = sum;
+                    of_the_king[c1, c2] = sum;
+                    of_the_king[c2, c1] = sum;
                 }
             }
 
             return of_the_king;
+        }
+
+        /// <summary>
+        /// Calculate the mean row.  This returns one value for each column!
+        /// </summary>
+        /// <param name="m"></param>
+        /// <returns></returns>
+        public static Vector<float> MeanRow(this Matrix<float> m)
+        {
+            return m.ColumnSums() / m.RowCount;
+        }
+
+        /// <summary>
+        /// Calculate the mean column.  This returns one value for each row!
+        /// </summary>
+        /// <param name="m"></param>
+        /// <returns></returns>
+        public static Vector<float> MeanColumn(this Matrix<float> m)
+        {
+            return m.RowSums() / m.ColumnCount;
+        }
+
+        /// <summary>
+        /// Return a new matrix where each column of the original matrix is summed with v
+        /// </summary>
+        /// <param name="m"></param>
+        /// <param name="v"></param>
+        /// <returns></returns>
+        public static Matrix<float> AddColumn(this Matrix<float> m, Vector<float> v)
+        {
+            if (m.RowCount != v.Count) throw new Exception("SubtractColumn Mismatch");
+
+            var mat = m.Clone();
+
+            int rows = m.RowCount;
+            int cols = m.ColumnCount;
+
+            for (int c = 0; c < cols; c++)
+            {
+                for (int r = 0; r < rows; r++)
+                {
+                    mat[r, c] -= v[r];
+                }
+            }
+
+            return mat;
+        }
+
+        /// <summary>
+        /// Return a new matrix where each row of the original matrix is summed with v
+        /// </summary>
+        /// <param name="m"></param>
+        /// <param name="v"></param>
+        /// <returns></returns>
+        public static Matrix<float> AddRow(this Matrix<float> m, Vector<float> v)
+        {
+            if (m.ColumnCount != v.Count) throw new Exception("SubtractRow Mismatch");
+            var mat = m.Clone();
+
+            int rows = m.RowCount;
+            int cols = m.ColumnCount;
+
+            for (int c = 0; c < cols; c++)
+            {
+                for (int r = 0; r < rows; r++)
+                {
+                    mat[r, c] += v[c];
+                }
+            }
+
+            return mat;
+        }
+
+
+        /// <summary>
+        /// Return a new matrix where each row of the original matrix is multiplied with v
+        /// </summary>
+        /// <param name="m"></param>
+        /// <param name="v"></param>
+        /// <returns></returns>
+        public static Matrix<float> MultiplyRow(this Matrix<float> m, Vector<float> v)
+        {
+            if (m.ColumnCount != v.Count) throw new Exception("SubtractRow Mismatch");
+            var mat = m.Clone();
+
+            int rows = m.RowCount;
+            int cols = m.ColumnCount;
+
+            for (int c = 0; c < cols; c++)
+            {
+                for (int r = 0; r < rows; r++)
+                {
+                    mat[r, c] *= v[c];
+                }
+            }
+
+            return mat;
+        }
+
+        /// <summary>
+        /// Calculate the standard deviation of the rows.  This returns one value for each column!
+        /// </summary>
+        /// <param name="m"></param>
+        /// <returns></returns>
+        public static Vector<float> StandardDeviationRow(this Matrix<float> m, bool zero_mean = false)
+        {
+            int cols = m.ColumnCount;
+
+            var stds = Vector<float>.Build.Dense(cols);
+
+            if (zero_mean)
+            {
+                for (int c = 0; c < cols; c++)
+                    stds[c] = StandardDeviationZeroMean(m.Column(c));
+            }
+            else
+            {
+                for (int c = 0; c < cols; c++)
+                    stds[c] = StandardDeviation(m.Column(c));
+            }
+
+            return stds;
+        }
+
+        private static float StandardDeviation(IEnumerable<float> vector)
+        {
+            float mean = 0;
+            int count = 0;
+
+            foreach (var v in vector)
+            {
+                count++;
+                mean += v;
+            }
+
+            if (count < 2) return float.NaN;
+
+            mean /= count;
+
+            float std = 0;
+
+            foreach (var v in vector)
+            {
+                float diff = v - mean;
+                std += diff * diff;
+            }
+
+            std /= count - 1;
+
+            return (float)Math.Sqrt(std);
+        }
+
+        private static float StandardDeviationZeroMean(IEnumerable<float> vector)
+        {
+            int count = 0;
+
+            float std = 0;
+
+            foreach (var v in vector)
+            {
+                count++;
+                std += v * v;
+            }
+
+            if (count < 2) return float.NaN;
+
+            std /= count - 1;
+
+            return (float)Math.Sqrt(std);
         }
     }
 }
