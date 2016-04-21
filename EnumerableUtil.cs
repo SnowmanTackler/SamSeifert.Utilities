1b57e3050426f73abebdc3672fda0a409a3251b1
Sam Seifert <seifes1@gmail.com>
2016-04-21 10:31:18 -0400

    Sub Array

--- EnumerableUtil.cs
+++ EnumerableUtil.cs
@@ -57,5 +57,21 @@ namespace SamSeifert.Utilities
                     ret++;
             return ret;
         }
+
+        /// <summary>
+        /// Get a sub array from data!
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="data"></param>
+        /// <param name="index"></param>
+        /// <param name="length"></param>
+        /// <returns></returns>
+        public static T[] SubArray<T>(this T[] data, int index, int length)
+        {
+            T[] result = new T[length];
+            Array.Copy(data, index, result, 0, length);
+            return result;
+        }
+
     }
 }
