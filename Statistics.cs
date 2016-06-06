0aac2ef41b40f2a3b8b47731ad69cc9d57c8bb61
Sam Seifert <seifes1@gmail.com>
2016-06-06 01:23:30 -0400

    Gaussian Double

--- Statistics.cs
+++ Statistics.cs
@@ -24,6 +24,21 @@ namespace SamSeifert.Utilities
         }
 
         /// <summary>
+        /// Returns a zero mean, 1 standard deviation gaussian.
+        /// </summary>
+        /// <param name="std"></param>
+        /// <param name="rand"></param>
+        /// <returns></returns>
+        public static Double NextGaussian(Random rand, Double std = 1.0f, Double mean = 0.0f)
+        {
+            double u1 = rand.NextDouble(); //these are uniform(0,1) random doubles
+            double u2 = rand.NextDouble();
+            double randStdNormal = Math.Sqrt(-2.0 * Math.Log(u1)) * Math.Sin(2.0 * Math.PI * u2);
+            return mean + std * randStdNormal;
+            // double randNormal = mean + stdDev * randStdNormal;
+        }
+
+        /// <summary>
         /// Returns -1 to 1
         /// </summary>
         /// <param name="std"></param>
