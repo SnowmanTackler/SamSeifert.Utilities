8b7139b8d865942ea19ced4a4deb1c6328db1f58
Sam Seifert <seifes1@gmail.com>
2016-06-27 01:20:40 -0400

    DirectShow Camera Working With Render

--- Samples/Camera/MainForm.Designer.cs
+++ Samples/Camera/MainForm.Designer.cs
@@ -28,49 +28,25 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.pictureBox1 = new System.Windows.Forms.PictureBox();
-            this.buttonResolution = new System.Windows.Forms.Button();
-            this.buttonAdvanced = new System.Windows.Forms.Button();
+            this.buttonCameraSettings = new System.Windows.Forms.Button();
             this.buttonSave = new System.Windows.Forms.Button();
             this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
             this.labelFPS = new System.Windows.Forms.Label();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.comboBoxCameraList = new System.Windows.Forms.ComboBox();
+            this.comboBoxResolutionList = new System.Windows.Forms.ComboBox();
+            this.panel1 = new System.Windows.Forms.Panel();
             this.SuspendLayout();
             // 
-            // pictureBox1
+            // buttonCameraSettings
             // 
-            this.pictureBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
-                        | System.Windows.Forms.AnchorStyles.Left)
-                        | System.Windows.Forms.AnchorStyles.Right)));
-            this.pictureBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
-            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
-            this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(640, 480);
-            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
-            this.pictureBox1.TabIndex = 0;
-            this.pictureBox1.TabStop = false;
-            // 
-            // buttonResolution
-            // 
-            this.buttonResolution.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
-            this.buttonResolution.Location = new System.Drawing.Point(12, 498);
-            this.buttonResolution.Name = "buttonResolution";
-            this.buttonResolution.Size = new System.Drawing.Size(129, 23);
-            this.buttonResolution.TabIndex = 1;
-            this.buttonResolution.Text = "Resolution";
-            this.buttonResolution.UseVisualStyleBackColor = true;
-            this.buttonResolution.Click += new System.EventHandler(this.buttonResolution_Click);
-            // 
-            // buttonAdvanced
-            // 
-            this.buttonAdvanced.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
-            this.buttonAdvanced.Location = new System.Drawing.Point(12, 527);
-            this.buttonAdvanced.Name = "buttonAdvanced";
-            this.buttonAdvanced.Size = new System.Drawing.Size(129, 23);
-            this.buttonAdvanced.TabIndex = 2;
-            this.buttonAdvanced.Text = "Advanced Settings";
-            this.buttonAdvanced.UseVisualStyleBackColor = true;
-            this.buttonAdvanced.Click += new System.EventHandler(this.buttonAdvanced_Click);
+            this.buttonCameraSettings.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.buttonCameraSettings.Location = new System.Drawing.Point(12, 473);
+            this.buttonCameraSettings.Name = "buttonCameraSettings";
+            this.buttonCameraSettings.Size = new System.Drawing.Size(129, 23);
+            this.buttonCameraSettings.TabIndex = 1;
+            this.buttonCameraSettings.Text = "Settings";
+            this.buttonCameraSettings.UseVisualStyleBackColor = true;
+            this.buttonCameraSettings.Click += new System.EventHandler(this.buttonCameraSettings_Click);
             // 
             // buttonSave
             // 
@@ -81,7 +57,6 @@
             this.buttonSave.TabIndex = 3;
             this.buttonSave.Text = "Save";
             this.buttonSave.UseVisualStyleBackColor = true;
-            this.buttonSave.Click += new System.EventHandler(this.buttonSave_Click);
             // 
             // saveFileDialog1
             // 
@@ -89,8 +64,8 @@
             // 
             // labelFPS
             // 
-            this.labelFPS.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)
-                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.labelFPS.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
             this.labelFPS.Location = new System.Drawing.Point(147, 498);
             this.labelFPS.Name = "labelFPS";
             this.labelFPS.Size = new System.Drawing.Size(370, 52);
@@ -98,32 +73,63 @@
             this.labelFPS.Text = "FPS";
             this.labelFPS.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
-            // Form1
+            // comboBoxCameraList
+            // 
+            this.comboBoxCameraList.FormattingEnabled = true;
+            this.comboBoxCameraList.Location = new System.Drawing.Point(12, 502);
+            this.comboBoxCameraList.Name = "comboBoxCameraList";
+            this.comboBoxCameraList.Size = new System.Drawing.Size(129, 21);
+            this.comboBoxCameraList.TabIndex = 5;
+            this.comboBoxCameraList.SelectedIndexChanged += new System.EventHandler(this.comboBoxCameraList_SelectedIndexChanged);
+            // 
+            // comboBoxResolutionList
+            // 
+            this.comboBoxResolutionList.FormattingEnabled = true;
+            this.comboBoxResolutionList.Location = new System.Drawing.Point(12, 529);
+            this.comboBoxResolutionList.Name = "comboBoxResolutionList";
+            this.comboBoxResolutionList.Size = new System.Drawing.Size(129, 21);
+            this.comboBoxResolutionList.TabIndex = 6;
+            this.comboBoxResolutionList.SelectedIndexChanged += new System.EventHandler(this.comboBoxResolutionList_SelectedIndexChanged);
+            // 
+            // panel1
+            // 
+            this.panel1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.panel1.Location = new System.Drawing.Point(12, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(640, 455);
+            this.panel1.TabIndex = 7;
+            // 
+            // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(664, 562);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.comboBoxResolutionList);
+            this.Controls.Add(this.comboBoxCameraList);
             this.Controls.Add(this.labelFPS);
             this.Controls.Add(this.buttonSave);
-            this.Controls.Add(this.buttonAdvanced);
-            this.Controls.Add(this.buttonResolution);
-            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.buttonCameraSettings);
             this.Enabled = false;
-            this.Name = "Form1";
+            this.Name = "MainForm";
             this.Text = "Form1";
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.FormMain_FormClosed);
+            this.Load += new System.EventHandler(this.FormCameraControlTool_Load);
             this.ResumeLayout(false);
 
         }
 
         #endregion
-
-        private System.Windows.Forms.PictureBox pictureBox1;
-        private System.Windows.Forms.Button buttonResolution;
-        private System.Windows.Forms.Button buttonAdvanced;
+        private System.Windows.Forms.Button buttonCameraSettings;
         private System.Windows.Forms.Button buttonSave;
         private System.Windows.Forms.SaveFileDialog saveFileDialog1;
         private System.Windows.Forms.Label labelFPS;
+        private System.Windows.Forms.ComboBox comboBoxCameraList;
+        private System.Windows.Forms.ComboBox comboBoxResolutionList;
+        private System.Windows.Forms.Panel panel1;
     }
 }
 
--- Samples/Camera/MainForm.cs
+++ Samples/Camera/MainForm.cs
@@ -1,87 +1,238 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading;
 using System.Windows.Forms;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Drawing.Drawing2D;
+using System.Drawing.Text;
+using System.Runtime.InteropServices.ComTypes;
 
-using SamSeifert.CSCV.Cameras;
+
+using DirectShowLib;
+using Camera_NET;
+using Camera = SamSeifert.CSCV.Cameras.Camera;
 
 namespace SampleProject
 {
     public partial class MainForm : Form
     {
+        // Camera choice
+        private CameraChoice _CameraChoice = new CameraChoice();
+        private Camera _Camera;
+
         public MainForm()
         {
-            Console.WriteLine(Guid.NewGuid().ToString());
             InitializeComponent();
-            Application.Idle += new EventHandler(this.Application_Idle);
         }
 
-        private bool notStarted = true;
-
-        private Camera webCamCapture1 = new Camera();
-
-        private int lastTime = System.Environment.TickCount;
-        private int count = 0;
-
-        public void Application_Idle(object sender, EventArgs e)
+        public void CloseCamera()
         {
-            if (this.notStarted)
+            if (_Camera != null)
             {
-                this.webCamCapture1.Start(this.Handle.ToInt32());
-                this.notStarted = false;
-                this.Enabled = true;
+                _Camera.StopGraph();
+                _Camera.CloseAll();
+                _Camera.Dispose();
+                _Camera = null;
+            }
+        }
+
+        // Camera object
+        //private Camera _Camera;
+
+
+
+        // On load of Form
+        private void FormCameraControlTool_Load(object sender, EventArgs e)
+        {
+            // Fill camera list combobox with available cameras
+            FillCameraList();
+
+            // Select the first one
+            if (comboBoxCameraList.Items.Count > 0)
+            {
+                comboBoxCameraList.SelectedIndex = 0;
+            }
+
+            // Fill camera list combobox with available resolutions
+            FillResolutionList();
+        }
+
+        // On close of Form
+        private void FormMain_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            this.CloseCamera();
+        }
+
+        #region Camera creation and destroy
+
+        // Set current camera to camera_device
+        private void SetCamera(IMoniker moniker, Resolution resolution)
+        {
+            try
+            {
+                // Makes all magic with camera and DirectShow graph
+                // Close current if it was opened
+                CloseCamera();
+
+                if (moniker == null)
+                    return;
+
+                // Create camera object
+                _Camera = new Camera();
+
+                string _DirectShowLogFilepath = string.Empty;
+
+                if (!string.IsNullOrEmpty(_DirectShowLogFilepath))
+                    _Camera.DirectShowLogFilepath = _DirectShowLogFilepath;
+
+                // select resolution
+                //ResolutionList resolutions = Camera.GetResolutionList(moniker);
+
+                if (resolution != null)
+                {
+                    _Camera.Resolution = resolution;
+                }
+
+                // Initialize
+                _Camera.Initialize(this.panel1, moniker);
+
+                Console.WriteLine("ARg 1");
+                // Build and Run graph
+                _Camera.BuildGraph();
+                Console.WriteLine("ARg2");
+                _Camera.RunGraph();
+                Console.WriteLine("ARg3");
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(e.Message, @"Error while running camera");
+            }
+        }
+
+        #endregion
+
+        #region Functions Buttons
+
+        private void buttonSnapshotNextSourceFrame_Click(object sender, EventArgs e)
+        {
+            if (this._Camera == null) return;
+
+
+            Bitmap bitmap = null;
+            try
+            {
+                bitmap = this._Camera.SnapshotSourceImage();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, @"Error while getting a snapshot");
+            }
+
+            if (bitmap == null)
+                return;
+        }
+
+        private void buttonCameraSettings_Click(object sender, EventArgs e)
+        {
+            if (this._Camera == null) return;
+
+            Camera.DisplayPropertyPage_Device(this._Camera.Moniker, this.Handle);
+        }
+
+        #endregion
+
+        
+        #region Camera and resolution selection
+
+        private void comboBoxCameraList_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (comboBoxCameraList.SelectedIndex < 0)
+            {
+                this.CloseCamera();
             }
             else
             {
-                Bitmap b = this.webCamCapture1.capture();
-
-                if (b != null)
-                {
-                    if (this.pictureBox1.Image != null) this.pictureBox1.Image.Dispose();
-                    this.pictureBox1.Image = b;
-
-
-                    this.count++;
-                    int newTime = System.Environment.TickCount;
-                    int elapsedTime = newTime - this.lastTime;
-                    if (elapsedTime > 500)
-                    {
-                        this.labelFPS.Text = "FPS: " + (count * 1000.0 / elapsedTime).ToString("00.0");
-                        this.lastTime = newTime;
-                        this.count = 0;
-                    }
-                }
-                else this.labelFPS.Text = "NULL";
+                // Set camera
+                this.SetCamera(_CameraChoice.Devices[comboBoxCameraList.SelectedIndex].Mon, null);
             }
+
+            FillResolutionList();
         }
 
-        private void buttonResolution_Click(object sender, EventArgs e)
+        private void comboBoxResolutionList_SelectedIndexChanged(object sender, EventArgs e)
         {
-            Console.WriteLine("RES");
-            this.webCamCapture1.configureResolution();
-        }
+            if (this._Camera == null) return;
 
-        private void buttonAdvanced_Click(object sender, EventArgs e)
-        {
-            Console.WriteLine("ADV");
-            this.webCamCapture1.configureSource();
-        }
-
-        private void buttonSave_Click(object sender, EventArgs e)
-        {
-            DialogResult dr = this.saveFileDialog1.ShowDialog();
-
-            if (dr == DialogResult.OK)
+            int comboBoxResolutionIndex = comboBoxResolutionList.SelectedIndex;
+            if (comboBoxResolutionIndex < 0)
             {
-                this.pictureBox1.Image.Save(
-                    this.saveFileDialog1.FileName,
-                    System.Drawing.Imaging.ImageFormat.Jpeg);
+                return;
+            }
+            ResolutionList resolutions = Camera.GetResolutionList(this._Camera.Moniker);
+
+            if (resolutions == null)
+                return;
+
+            if (comboBoxResolutionIndex >= resolutions.Count)
+                return; // throw
+
+            if (0 == resolutions[comboBoxResolutionIndex].CompareTo(this._Camera.Resolution))
+            {
+                // this resolution is already selected
+                return;
+            }
+
+            // Recreate camera
+            this.SetCamera(this._Camera.Moniker, resolutions[comboBoxResolutionIndex]);
+
+        }
+
+        private void FillResolutionList()
+        {
+            comboBoxResolutionList.Items.Clear();
+
+            if (this._Camera == null) return;
+
+            ResolutionList resolutions = Camera.GetResolutionList(this._Camera.Moniker);
+
+            if (resolutions == null)
+                return;
+
+
+            int index_to_select = -1;
+
+            for (int index = 0; index < resolutions.Count; index++)
+            {
+                comboBoxResolutionList.Items.Add(resolutions[index].ToString());
+
+                if (resolutions[index].CompareTo(this._Camera.Resolution) == 0)
+                {
+                    index_to_select = index;
+                }
+            }
+
+            // select current resolution
+            if (index_to_select >= 0)
+            {
+                comboBoxResolutionList.SelectedIndex = index_to_select;
             }
         }
+
+        private void FillCameraList()
+        {
+            comboBoxCameraList.Items.Clear();
+
+            _CameraChoice.UpdateDeviceList();
+
+            foreach (var camera_device in _CameraChoice.Devices)
+            {
+                comboBoxCameraList.Items.Add(camera_device.Name);
+            }
+        }
+
+        #endregion
+
+
+
+
     }
 }
