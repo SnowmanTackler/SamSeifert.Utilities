2ea71d2a820c406a9823e42560c58751802035c3
Sam Seifert <seifes1@gmail.com>
2016-10-15 16:29:44 -0400

    Was IPEndPoint

--- Networking/Destination.cs
+++ Networking/Destination.cs
@@ -8,33 +8,16 @@ using System.Threading.Tasks;
 
 namespace SamSeifert.Utilities.Networking
 {
-    public class Destination
+    public static class IPEndPointExtensions
     {
-        public readonly IPAddress _IP;
-        public readonly int _Port;
-        private readonly String _About;
-
-        public override string ToString()
-        {
-            return this._About;
-        }
-
-        public Destination(int port, IPAddress ip, String description = null)
-        {
-            this._Port = port;
-            this._IP = ip;
-            this._About = description == null ? (this._IP + ":" + this._Port) : description;
-        }
-
-
-        public static Destination Local(int port, String Description = null)
+        public static IPEndPoint Local(int port, String Description = null)
         {
             var host = Dns.GetHostEntry(Dns.GetHostName());
             foreach (var ip in host.AddressList)
             {
                 if (ip.AddressFamily == AddressFamily.InterNetwork)
                 {
-                    return new Destination(port, ip, Description);
+                    return new IPEndPoint(ip, port);
                 }
             }
             throw new Exception("Local IP Address Not Found!");
