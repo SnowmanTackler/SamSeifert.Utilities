97d031883383435dcbadf05437dd0925bf0b9ad2
Sam Seifert <seifes1@gmail.com>
2016-12-16 23:57:20 -1000

    Rotate those vecs

---
+++ Extensions.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using OpenTK;
+
+namespace SamSeifert.GLE
+{
+    public static class Extensions
+    {
+
+        public static Vector2 RotateClockwise(this Vector2 v, float angle_degrees)
+        {
+            float ct = SamSeifert.Utilities.UnitConverter.CosDegrees(angle_degrees);
+            float st = SamSeifert.Utilities.UnitConverter.SinDegrees(angle_degrees);
+            return new Vector2(
+                v.X * ct - v.Y * st,
+                v.Y * ct + v.X * st
+                );
+        }
+
+        public static Vector2 RotateCounterClockwise(this Vector2 v, float angle_degrees)
+        {
+            return (v.RotateClockwise(-angle_degrees));
+        }
+
+    }
+}
