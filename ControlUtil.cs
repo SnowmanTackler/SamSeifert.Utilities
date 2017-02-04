f0827c73e1a2f696814ad1596d2ed25c1b4cf682
Sam Seifert <seifes1@gmail.com>
2017-02-04 17:36:00 -0500

    Control Holder Default String

--- ControlUtil.cs
+++ ControlUtil.cs
@@ -11,16 +11,20 @@ namespace SamSeifert.Utilities
 {
     public class ControlHolder<T>
     {
+        private String _String;
         public T _Held;
 
         public override string ToString()
         {
-            return this._Held.ToString();
+            return this._String;
         }
 
-        public ControlHolder(T ai)
+        public ControlHolder(T ai, String s = null)
         {
             this._Held = ai;
+
+            if (s == null) this._String = this._Held.ToString();
+            else this._String = s;
         }
     }
 
