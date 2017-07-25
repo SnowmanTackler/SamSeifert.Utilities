7126417b791261daeda7d78e55bcb549a04d3654
Sam Seifert <seifes1@gmail.com>
2017-07-25 10:34:21 -0700

    Low Pass Filter Own Class

--- MathUtil.cs
+++ MathUtil.cs
@@ -175,43 +175,6 @@ namespace SamSeifert.Utilities
             {
             }
         }
-
-        public class LowPassFilter
-        {
-            private readonly float _Alpha;
-            private bool _First = true;
-            private float _Value = 0;
-
-            /// <summary>
-            /// New values multiplied by (alpha).  
-            /// Old Values multiplied by (alpha - 1).
-            /// </summary>
-            /// <param name="alpha"></param>
-            public LowPassFilter(float alpha = 0.1f)
-            {
-                this._Alpha = alpha;
-            }
-
-            public void Reset()
-            {
-                this._First = true;
-            }
-
-            public float Update(float input)
-            {
-                if (this._First)
-                {
-                    this._First = false;
-                    this._Value = input;
-                }
-                else
-                {
-                    this._Value *= (1 - this._Alpha);
-                    this._Value += this._Alpha * input;
-                }
-                return this._Value;
-            }
-        }
     }
 }
 
