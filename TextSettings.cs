6c9151706c7473a1c0c03eb28f5db9fe1caba82e
Sam Seifert <seifes1@gmail.com>
2018-01-02 22:39:54 -0500

    Text Settings

---
+++ TextSettings.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SamSeifert.Utilities
+{
+    public static class TextSettings
+    {
+        public static String Folder
+        {
+            get
+            {
+                var pa = System.IO.Path.Combine(
+                    Application.ExecutablePath,
+                    "..",
+                    "..",
+                    "..",
+                    "Settings");
+
+                return Path.GetFullPath(pa);
+            }
+        }
+
+        public static void Save(string file_name, string text)
+        {
+            Logger.WriteLine("File Saved: " + file_name);
+
+            var path = Path.Combine(Folder, file_name);
+
+            var dir = Path.GetDirectoryName(path);
+
+            if (!Directory.Exists(dir))
+                Directory.CreateDirectory(dir);
+
+            File.WriteAllText(path, text);
+        }
+
+        public static String Read(string file_name)
+        {
+            string path = Path.Combine(Folder, file_name);
+            if (File.Exists(path))
+                return File.ReadAllText(path);
+            else return null;
+        }
+    }
+}
