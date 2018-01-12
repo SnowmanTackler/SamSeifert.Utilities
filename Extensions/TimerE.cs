3c87a9254785af0aa96a62fca339167220952bd6
Sam Seifert <seifes1@gmail.com>
2018-01-12 16:33:43 -0500

    Timer Zero

---
+++ Extensions/TimerE.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SamSeifert.Utilities.Extensions
+{
+    public static class TimerE
+    {
+        /// <summary>
+        /// Also starts timer by enabling it.
+        /// </summary>
+        /// <param name="t"></param>
+        public static void Zero(this Timer t)
+        {
+            t.Enabled = false;
+            t.Enabled = true;
+        }
+    }
+}
