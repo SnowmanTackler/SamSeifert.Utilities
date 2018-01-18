eec849d88a191da013153fc9736a2fd1bcbd5fab
Sam Seifert <seifes1@gmail.com>
2018-01-18 01:10:04 -0500

    CLip Plane Static Variable

---
+++ GLR_StaticVariables.cs
@@ -0,0 +1,14 @@
+﻿using OpenTK;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SamSeifert.GLE
+{
+    public partial class GLR
+    {
+        public static Vector4 _ClippingPlane0 = Vector4.Zero;
+    }
+}
