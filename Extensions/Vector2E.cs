b9e7f7ad4ab9f4eb81c714e07f466660396c9750
Sam Seifert <seifes1@gmail.com>
2018-03-26 17:43:32 -0400

    Dedicated Extensions Now

---
+++ Extensions/Vector2E.cs
@@ -0,0 +1,18 @@
+﻿using OpenTK;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SamSeifert.GLE.Extensions
+{
+    public static class Vector2E
+    {
+        public static Vector2 Blend01(this float alpha, Vector2 value_at_0, Vector2 value_at_1)
+        {
+            return alpha * value_at_1 + (1 - alpha) * value_at_0;
+        }
+
+    }
+}
