aa67b260256d13fdaf50ae7857c0cb8e04bb4e97
Sam Seifert <seifes1@gmail.com>
2019-12-08 23:21:48 -0500

    [GLE] Hollow Cylinders

--- SamSeifert.GLE/CAD/Generator/Generic.cs
+++ SamSeifert.GLE/CAD/Generator/Generic.cs
@@ -113,7 +113,12 @@ namespace SamSeifert.GLE.CAD.Generator
             return CreateCylinder(c._Point1, c._Point2, c._Radius, section);
         }
 
-        public static CadObject CreateCylinder(Vector3 bot, Vector3 top, float radius, int section = 36)
+        public static CadObject CreateCylinder(
+            Vector3 bot,
+            Vector3 top,
+            float radius,
+            int section = 36
+            )
         {
             var norm = (bot - top).Normalized();
 
@@ -152,6 +157,58 @@ namespace SamSeifert.GLE.CAD.Generator
             co._CullFaceMode = OpenTK.Graphics.OpenGL.CullFaceMode.Back;
             return co;
         }
+
+        public static CadObject CreateCylinderHollow(
+            Vector3 bot,
+            Vector3 top,
+            float outerRadius,
+            float innerRadius,
+            int section = 36
+            )
+        {
+            var norm = (bot - top).Normalized();
+
+            Vector3 perp1, perp2;
+
+            {
+                perp1 = norm.Perpindicular();
+                perp2 = Vector3.Cross(perp1, norm).Normalized();
+            }
+
+            List<Vector3> vs = new List<Vector3>();
+            List<Vector3> ns = new List<Vector3>();
+
+            for (int i = 0; i < section; i++)
+            {
+                var angle1 = (i + 0) * Math.PI * 2 / section;
+                var angle2 = (i + 1) * Math.PI * 2 / section;
+
+                var radial1 = perp1 * outerRadius * (float)Math.Cos(angle1) + perp2 * outerRadius * (float)Math.Sin(angle1);
+                var radial2 = perp1 * outerRadius * (float)Math.Cos(angle2) + perp2 * outerRadius * (float)Math.Sin(angle2);
+                var radial3 = perp1 * innerRadius * (float)Math.Cos(angle2) + perp2 * innerRadius * (float)Math.Sin(angle2);
+                var radial4 = perp1 * innerRadius * (float)Math.Cos(angle1) + perp2 * innerRadius * (float)Math.Sin(angle1);
+
+                Face(top + radial1, top + radial2, top + radial3, top + radial4, -norm, vs, ns);
+                Face(bot + radial4, bot + radial3, bot + radial2, bot + radial1, norm, vs, ns);
+
+                vs.AddRange(new Vector3[] { bot + radial2, top + radial1, bot + radial1 });
+                ns.AddRange(new Vector3[] { radial2.Normalized(), radial1.Normalized(), radial1.Normalized() });
+
+                vs.AddRange(new Vector3[] { bot + radial2, top + radial2, top + radial1 });
+                ns.AddRange(new Vector3[] { radial2.Normalized(), radial2.Normalized(), radial1.Normalized() });
+
+                vs.AddRange(new Vector3[] { bot + radial4, top + radial3, bot + radial3 });
+                ns.AddRange(new Vector3[] { -radial4.Normalized(), -radial3.Normalized(), -radial3.Normalized() });
+
+                vs.AddRange(new Vector3[] { bot + radial4, top + radial4, top + radial3 });
+                ns.AddRange(new Vector3[] { -radial4.Normalized(), -radial4.Normalized(), -radial3.Normalized() });
+            }
+
+            var co = new CadObject(vs.ToArray(), ns.ToArray(), "Cylinder");
+            co._CullFaceMode = OpenTK.Graphics.OpenGL.CullFaceMode.Back;
+            return co;
+        }
+
 
         public static CadObject CreateCylinders(Vector3[] fiberPath, float radius, int section = 36)
         {
