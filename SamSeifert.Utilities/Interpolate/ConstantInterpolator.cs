f8ea67418a33245aba2b9997c3861169d8642e34
Sam Seifert <seifes1@gmail.com>
2019-12-26 21:19:09 -0500

    [Util] Interpolator

---
+++ SamSeifert.Utilities/Interpolate/ConstantInterpolator.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SamSeifert.Utilities.Interpolate
+{
+    public class ConstantInterpolator : Interpolator
+    {
+        private readonly double Value;
+
+        public ConstantInterpolator(double value)
+        {
+            this.Value = value;
+        }
+
+        public double evaluate(int currentTime)
+        {
+            return this.Value;
+        }
+    }
+}
