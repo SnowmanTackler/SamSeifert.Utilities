8b26c2159723776b381cf29c1effa2df288413d0
Sam Seifert <seifes1@gmail.com>
2020-02-09 11:20:31 -0500

    [Util] LockWrapperStruct

--- SamSeifert.Utilities/Concurrent/LockWrapper.cs
+++ SamSeifert.Utilities/Concurrent/LockWrapper.cs
@@ -1,33 +1,24 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Threading;
-using System.Threading.Tasks;
 
-namespace SamSeifert.Utilities.Concurrent
-{
+namespace SamSeifert.Utilities.Concurrent {
     /// <summary>
     /// Wraps a lock and gives it helpful methods like Pulse, PulseAll, and Wait.
     /// </summary>
     public class MonitorWrapper {
         private Object Lock;
-        public MonitorWrapper(object Lock)
-        {
+        public MonitorWrapper(object Lock) {
             this.Lock = Lock;
         }
-        public void Pulse()
-        {
+        public void Pulse() {
             Monitor.Pulse(this.Lock);
         }
 
-        public void PulseAll()
-        {
+        public void PulseAll() {
             Monitor.PulseAll(this.Lock);
         }
 
-        public void Wait(int millisecondsTimeout = int.MaxValue)
-        {
+        public void Wait(int millisecondsTimeout = int.MaxValue) {
             Monitor.Wait(this.Lock, millisecondsTimeout);
         }
     }
@@ -37,34 +28,48 @@ namespace SamSeifert.Utilities.Concurrent
     /// "Access" methods.
     /// </summary>
     /// <typeparam name="Mutable"></typeparam>
-    public class LockWrapper<Mutable>
-    {
+    public class LockWrapper<Mutable> {
         private readonly Mutable Instance;
         private readonly MonitorWrapper MonitorLock;
         private readonly object Lock = new Object();
 
-        public LockWrapper(Mutable mutable)
-        {
+        public LockWrapper(Mutable mutable) {
             this.Instance = mutable;
             this.MonitorLock = new MonitorWrapper(Lock);
         }
 
-        public void Access(Action<Mutable, MonitorWrapper> action)
-        {
-            lock (this.Lock)
-            {
+        public void Access(Action<Mutable, MonitorWrapper> action) {
+            lock (this.Lock) {
                 action(this.Instance, this.MonitorLock);
             }
         }
 
-        public T Access<T>(Func<Mutable, MonitorWrapper, T> action)
-        {
-            lock (this.Lock)
-            {
+        public T Access<T>(Func<Mutable, MonitorWrapper, T> action) {
+            lock (this.Lock) {
                 T ret = action(this.Instance, this.MonitorLock);
                 return ret;
             }
         }
+    }
 
+    public class LockWrapperStruct<T> where T : struct {
+        private T Instance;
+        private readonly object Lock = new Object();
+
+        public LockWrapperStruct(T m) {
+            this.Instance = m;
+        }
+
+        public void Set(T m) {
+            lock (this.Lock) {
+                this.Instance = m;
+            }
+        }
+
+        public T Get() {
+            lock (this.Lock) {
+                return this.Instance;
+            }
+        }
     }
 }
