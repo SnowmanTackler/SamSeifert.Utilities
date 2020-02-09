2e4f5a69f868e58c5ed3f420136534c845caaec1
Sam Seifert <seifes1@gmail.com>
2020-02-08 21:46:15 -0500

    [GLE.Forms] Screen Shot Saver + Refactor

--- SamSeifert.GLE.Forms/ScreenShot.cs
+++ SamSeifert.GLE.Forms/ScreenShot.cs
@@ -13,7 +13,7 @@ namespace SamSeifert.GLE {
         /// </summary>
         /// <param name="glControl"></param>
         /// <returns></returns>
-        public static Bitmap TakeScreenshot(this OpenTK.GLControl glControl) {
+        public static Bitmap TakeScreenshot(this OpenTK.GLControl glControl, Rectangle? rect = null) {
             if (GraphicsContext.CurrentContext == null)
                 throw new GraphicsContextMissingException();
             int w = glControl.ClientSize.Width;
@@ -23,7 +23,22 @@ namespace SamSeifert.GLE {
                 bmp.LockBits(glControl.ClientRectangle, System.Drawing.Imaging.ImageLockMode.WriteOnly, System.Drawing.Imaging.PixelFormat.Format24bppRgb);
             GL.ReadPixels(0, 0, w, h, PixelFormat.Bgr, PixelType.UnsignedByte, data.Scan0);
             bmp.UnlockBits(data);
-            return bmp;
+
+            bmp.RotateFlip(RotateFlipType.RotateNoneFlipY);
+
+            if (!rect.HasValue)
+                return bmp;
+
+            Bitmap target = new Bitmap(rect.Value.Width, rect.Value.Height);
+            using (Graphics g = Graphics.FromImage(target)) {
+                g.DrawImage(bmp, new Rectangle(0, 0, target.Width, target.Height),
+                                 rect.Value,
+                                 GraphicsUnit.Pixel);
+            }
+
+            bmp.Dispose();
+
+            return target;
         }
     }
 }
